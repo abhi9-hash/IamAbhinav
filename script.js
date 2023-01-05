d80654e43957ec815ca0614b5c5a482c9bc420b5
abhnv_pthk <coolabhinav57@gmail.com>
2023-01-05 18:21:07 +0530

    Updated info

--- script.js
+++ script.js
@@ -55,12 +55,12 @@ $(document).ready(function(){
 
   //loading screen
 
-  const loader = document.querySelector(".loader");
-         window.onload = function(){
-           setTimeout(function(){
-             loader.style.opacity = "0";
-             setTimeout(function(){
-               loader.style.display = "none";
-             }, 500);
-           },1500);
-         }
+  // const loader = document.querySelector(".loader");
+  //        window.onload = function(){
+  //          setTimeout(function(){
+  //            loader.style.opacity = "0";
+  //            setTimeout(function(){
+  //              loader.style.display = "none";
+  //            }, 500);
+  //          },1500);
+  //        }
